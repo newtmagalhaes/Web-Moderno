d78d3acd243eb1f6e9731461215a58a666b20b0e
newtmagalhaes <aniltonmagalhaes@gmail.com>
2020-05-07 18:43:45 -0300

    Adicionando aulas 1 e 2

---
+++ WebModerno/01-fundamentos/000-comentarios.js
@@ -0,0 +1,17 @@
+// Comentários em JavaScript
+
+/** 
+ * Há duas formas de criar comentários
+ * em JavaScript:
+ * 
+ * - Utilizando duas barras: " // "
+ *      o que está à direita das barras é comentário;
+ *      isto é um comentário de 1 (uma) linha apenas;
+ * 
+ * - Utilizando barra e asterísco para abrir
+ *   e asterisco barra para fechar
+ *      assim, o que estiver entre os dois pares será comentário;
+ *      isto permite comentários de multiplas linhas;
+*/
+
+// Esta linha também é comentário.
