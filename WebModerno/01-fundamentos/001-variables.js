b553db30b7aa8c8aa80dc9d745152b14795e1ad3
newtmagalhaes <aniltonmagalhaes@gmail.com>
2020-05-09 18:08:04 -0300

    Adicionando aula 002 sobre declaração de variáveis

--- WebModerno/01-fundamentos/001-variables.js
+++ WebModerno/01-fundamentos/001-variables.js
@@ -1,3 +1,5 @@
+// Declarando variáveis em JavaScript
+
 /**
  * Há 4 formas de declarar variáveis,
  * e 3 delas utilizam palavras reservadas;
@@ -17,21 +19,21 @@ var variavel3 = "String3";
 // Declarando com let:
 let variavel4 = "String4";
 
-/** 
+/**
  * Obs: let e var são semelhantes,
  * porém, é sempre mais aconselhável
  * utilizar o let pra evitar problemas
  * devido a "estranhezas" do var;
- * 
+ *
  * Obs: inicialmente, quando uma
  * variável é declarada o dado
  * inicial que ela contém é undefined;
- * 
- * veja melhor explicado no 'Guia do mundo tecnológico';
+ *
  */
 
-console.log("\n",
-    "O que está armazenado na variável1 é: ", variavel1, "do tipo", typeof variavel1,"\n",
-    "O que está armazenado na variável2 é: ", variavel2, "do tipo", typeof variavel2,"\n",
-    "O que está armazenado na variável3 é: ", variavel3, "do tipo", typeof variavel3,"\n",
-    "O que está armazenado na variável4 é: ", variavel4, "do tipo", typeof variavel4);
+console.log(
+  `O que está armazenado na variável1 é: ${variavel1} do tipo ${typeof variavel1};
+    O que está armazenado na variável1 é: ${variavel2} do tipo ${typeof variavel2};
+    O que está armazenado na variável1 é: ${variavel3} do tipo ${typeof variavel3};
+    O que está armazenado na variável1 é: ${variavel4} do tipo ${typeof variavel4};`
+);
