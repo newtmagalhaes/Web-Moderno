9912ee0f6b2d1ee22bd2fc6699306a4ed00ab20f
newtmagalhaes <aniltonmagalhaes@gmail.com>
2020-05-13 19:32:11 -0300

    Adicionando aula 015 sobre operação de atribuição

---
+++ WebModerno/01-fundamentos/015-atribuicao.js
@@ -0,0 +1,26 @@
+// Operador de Atribuição em JavaScript
+
+/**
+ * A atribuição em JavaScript
+ * pode ser por valor (cópia)
+ * ou por referência, dependendo
+ * do dado à direita do operador
+ */
+
+let var1 = 42; // Atribuição: atribui o dado à direita à variável à esquerda;
+console.log("var1 é:", var1);
+
+var1 += 24; // Atribuição aditiva: equivalente a var1 = var1 + 24;
+console.log("var1 += 24 é:", var1);
+
+var1 -= 24; // Atribuição subtrativa: equivalente a var1 = var1 - 24;
+console.log("var1 -= 24 é:", var1);
+
+var1 *= 2; // Atribuição multiplicativa: equivalente a var1 = var1 * 2;
+console.log("var1 *= 2 é:", var1);
+
+var1 /= 2; // Atribuição divisiva: equivalente a var1 = var1 / 24;
+console.log("var1 /= 2 é:", var1);
+
+var1 %= 4; // Atribuição modular: equivalente a var1 = var1 % 4;
+console.log("var1 %= 4 é:", var1);
