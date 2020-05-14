119b7016c37c6d6e500b86e70441924edcc4affb
newtmagalhaes <aniltonmagalhaes@gmail.com>
2020-05-14 19:40:16 -0300

    Adicionando aula 020 sobre operadores relacionais

---
+++ WebModerno/01-fundamentos/020-relacionais.js
@@ -0,0 +1,16 @@
+// Operadores relacionais em JavaScript
+
+// Operadores relacionais retornam Booleans
+console.log(`operadores bínários:
+equivalência:           '1' == 1 : ${"1" == 1};
+equivalência estrita:   '1' === 1: ${"1" === 1};
+diferença:              '1' != 1 : ${"1" != 1};
+diferença estrita:      '1' !== 1: ${"1" !== 1};
+menor que:              1 < 0    : ${1 < 0};
+maior que:              1 > 0    : ${1 > 0};
+maior ou igual:         1 >= 0   : ${1 >= 0};
+menor ou igual:         1 <= 0   : ${1 <= 0};`);
+/**
+ * Atenção à comparação de dados não primitivos
+ * pois o que é comparado é o endereço de memória!!!
+ */
