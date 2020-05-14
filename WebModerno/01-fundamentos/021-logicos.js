412e7bfb5abb9e206b19a2bf825a21c47e3a2c26
newtmagalhaes <aniltonmagalhaes@gmail.com>
2020-05-14 19:40:41 -0300

    Adicionando aula 021 sobre operadores lógicos

---
+++ WebModerno/01-fundamentos/021-logicos.js
@@ -0,0 +1,8 @@
+// Operadores Lógicos em JavaScript
+
+// Operadores Lógicos retornam booleanos
+console.log(`Operadores Lógicos:
+OR: false || false  : ${false || false};
+AND: true && true   : ${true && true};
+XOR: false != true  : ${false != true};
+NOT: !true          : ${!true};`);
