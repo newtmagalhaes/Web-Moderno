5a7be6588b1e5e4cdd2ca71046af5a74662aa7a9
newtmagalhaes <aniltonmagalhaes@gmail.com>
2020-07-05 19:04:28 -0300

    Adicionando aula 036 sobre funções autoinvocadas

---
+++ WebModerno/03-funcao/036-iife.js
@@ -0,0 +1,12 @@
+// IIFE em JavaScript
+
+/**
+ * IIFE: Immediately Invoked Function Expression
+ * Funções chamadas logo após a sua declaração
+ * É útil para fugir do escopo global no Browser
+ */
+
+(function () {
+  console.log("É preciso declarar a função entre parêntesis");
+  console.log("por exemplo: (function name (param) {code})()");
+})();
